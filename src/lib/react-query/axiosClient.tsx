eff3b6c87191cf131514a32a31d3772db25d193f
deepanshu-fe <deepanshu.tyagi@autviz.in>
2025-12-10 17:38:06 +0530

    add new url

--- src/lib/react-query/axiosClient.tsx
+++ src/lib/react-query/axiosClient.tsx
@@ -1,65 +1,6 @@
-// // lib/axios.js
-// import axios from 'axios';
-// import { useAuthStore } from '../../stores/store';
-
-// const instance = axios.create({
-//   timeout: 90000,
-//   withCredentials: true,
-// });
-
-// const BaseURL = 'https://crwd.autviz.com';
-
-// console.log(useAuthStore.getState().token , 'token in axiosClient');
-
-
-// instance.interceptors.request.use((config) => {
-//   const token = useAuthStore.getState().token?.access_token;
-//   config.baseURL = BaseURL;
-
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   console.log(token, 'token in interceptor');
-
-//   return config;
-// });
-
-// instance.interceptors.response.use(
-//   (response) => {
-//     console.log('Response:', response.status, response.data);
-//     return response;
-//   },
-//   (error) => {
-    
-//     console.log('Error Response:', error.response?.status, error.response?.data);
-    
-//     if (error.response?.status === 403) {
-//       console.log('403 Forbidden - Token may be expired or invalid');
-//       axios.post(BaseURL + '/auth/cognito/refresh/', {
-//         refresh_token: useAuthStore.getState().token?.refresh_token,
-//       }).then((response) => {
-//         console.log('Refresh token response:', response.data);
-//         useAuthStore.getState().setToken(response.data);
-//       }).catch((error) => {
-//         console.log('Refresh token error:', error);
-//         // useAuthStore.getState().setToken({});
-//         // navigate('/onboarding');
-//       });
-//     }
-    
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default instance;
-
-
-
 // lib/axios.js
-import axios from 'axios';
-import { useAuthStore } from '../../stores/store';
+import axios from "axios";
+import { useAuthStore } from "../../stores/store";
 
 const instance = axios.create({
   timeout: 90000,
@@ -69,7 +10,7 @@ const instance = axios.create({
 // const BaseURL = 'https://crwd.autviz.com';
 // const BaseURL = 'https://api.crwdfund.org';
 // const BaseURL = 'http://ec2-65-0-54-143.ap-south-1.compute.amazonaws.com:8200';
-const BaseURL = 'https://crwd.autviz.com';
+const BaseURL = "https://crwd.autviz.com";
 
 // 🔹 Request Interceptor
 instance.interceptors.request.use((config) => {
@@ -93,7 +34,7 @@ instance.interceptors.response.use(
     if (error.response?.status === 403 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      console.log('403 Forbidden - Trying to refresh token');
+      console.log("403 Forbidden - Trying to refresh token");
 
       try {
         const refresh_token = useAuthStore.getState().token?.refresh_token;
@@ -104,19 +45,26 @@ instance.interceptors.response.use(
           username,
         });
 
-        console.log('Refresh token response:', res.data);
+        console.log("Refresh token response:", res.data);
 
         // Save new token
-        useAuthStore.getState().setToken({ access_token: res.data.access_token, refresh_token: refresh_token });
+        useAuthStore
+          .getState()
+          .setToken({
+            access_token: res.data.access_token,
+            refresh_token: refresh_token,
+          });
 
         // Update Authorization header and retry the failed request
         originalRequest.headers.Authorization = `Bearer ${res.data.access_token}`;
         return instance(originalRequest);
       } catch (refreshError) {
-        console.log('Refresh token failed:', refreshError);
-        useAuthStore.getState().setToken({ access_token: '', refresh_token: '' });
+        console.log("Refresh token failed:", refreshError);
+        useAuthStore
+          .getState()
+          .setToken({ access_token: "", refresh_token: "" });
         useAuthStore.getState().setUser({});
-        window.location.href = '/login';
+        window.location.href = "/login";
         // You can trigger navigation or logout here if needed
       }
     }
